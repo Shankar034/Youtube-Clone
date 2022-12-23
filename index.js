9af0c478677619727feac01cdc2351e8674dacb1
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-12-23 13:08:15 +0545

    Forth Commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 console.log("This is the JavaScript File")
 var menuIcon = document.querySelector('.menu-icon');
 var sidebar = document.querySelector('.sidebar');
+var container = document.querySelector('.container');
 
 
 menuIcon.onclick = function(){
     sidebar.classList.toggle('small-sidebar');
+    container.classList.toggle('large-container');
 }
+
